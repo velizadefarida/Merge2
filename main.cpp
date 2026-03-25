2bd77a133fad66a963976bb63e98eadf30e108b9
Farida <velizadefarida@gmail.com>
2026-03-25 20:35:45 +0300

    Basic merge

--- main.cpp
+++ main.cpp
@@ -1,5 +1,33 @@
-#include <iostream>
-
-int main()
+template<class T>
+T* merge(const T* a, size_t sa, const T* b, size_t sb, T* c)
 {
+    T* cur = c;
+    size_t i = 0, j = 0;
+    while (i < sa && j < sb)
+    {
+        if (a[i] < b[j])
+        {
+            *cur = a[i];
+            ++i;
+        }
+        else
+        {
+            *cur = b[j];
+            ++j;
+        }
+        ++cur;
+    }
+    while (i < sa)
+    {
+        *cur = a[i];
+        ++i;
+        ++cur;
+    }
+    while (j < sb)
+    {
+        *cur = b[j];
+        ++j;
+        ++cur;
+    }
+    return cur;
 }
