a5a9cf5119e668cc0815760f127bef731f1b7bc9
Farida <velizadefarida@gmail.com>
2026-03-25 20:44:50 +0300

    done

--- main.cpp
+++ main.cpp
@@ -1,33 +1,40 @@
+#include <cstddef>
+
 template<class T>
 size_t merge(const T* a, size_t sa, const T* b, size_t sb, T* c)
 {
-    T* cur = c;
-    size_t i = 0, j = 0;
+  T* cur = c;
+  size_t i = 0, j = 0;
+  try {
     while (i < sa && j < sb)
     {
-        if (a[i] < b[j])
-        {
-            *cur = a[i];
-            ++i;
-        }
-        else
-        {
-            *cur = b[j];
-            ++j;
-        }
-        ++cur;
+      if (a[i] < b[j])
+      {
+        *cur = a[i];
+        ++i;
+      }
+      else
+      {
+        *cur = b[j];
+        ++j;
+      }
+      ++cur;
     }
     while (i < sa)
     {
-        *cur = a[i];
-        ++i;
-        ++cur;
+      *cur = a[i];
+      ++i;
+      ++cur;
     }
     while (j < sb)
     {
-        *cur = b[j];
-        ++j;
-        ++cur;
+      *cur = b[j];
+      ++j;
+      ++cur;
     }
+  }
+  catch (...) {
     return static_cast<size_t>(cur - c);
+  }
+  return static_cast<size_t>(cur - c);
 }
